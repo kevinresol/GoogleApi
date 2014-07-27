2f09a0b90e0c7b4e7cd4b15b5c3ec09a27747b16
kevinresol <kevinresol@gmail.com>
2014-07-27 23:39:59 +0800

    MakeRestCall now will wait for ready and auto fetch token

--- GoogleApi.hx
+++ GoogleApi.hx
@@ -3,6 +3,7 @@ import flash.net.URLRequest;
 import openfl.events.Event;
 import openfl.events.IOErrorEvent;
 import openfl.net.URLLoader;
+import openfl.net.URLVariables;
 
 #if cpp
 import cpp.Lib;
@@ -46,6 +47,7 @@ class GoogleApi
 	
 	public static function getToken(scope:String):Surprise<String, Error>
 	{
+		//TODO cache
 		return Future.async(function(handler)
 		{
 			googleapi_get_token_jni( { handler:function(s:String) 
@@ -60,20 +62,33 @@ class GoogleApi
 	
 	public static function invalidateToken(token:String):Void
 	{
-		googleapi_invalidate_token_jni(
+		googleapi_invalidate_token_jni(token);
 	}
 	
-	public static function makeRestCall(url:String):Surprise<String, Error>
+	public static function makeRestCall(url:String, variables:URLVariables, scope:String):Surprise<String, Error>
 	{
-		return Future.async(function(handler)
+		//ready -> getToken -> load url
+		
+		//TODO cache
+		return ready.flatMap(function(ready)
 		{
-			
-			var request = new URLRequest(url);
-			var loader = new URLLoader();
-			loader.addEventListener(Event.COMPLETE, function(_) handler(Success(loader.data)));
-			loader.addEventListener(IOErrorEvent.IO_ERROR, function(e:IOErrorEvent) handler(Failure(new Error(e.toString()))));
-			//TODO add error listener
-			loader.load(request);
+			return getToken(scope).flatMap(function(outcome)			
+			{
+				try
+				{
+					variables.access_token = outcome.sure();
+					return Future.async(function(handler)
+					{
+						var request = new URLRequest(url + "?" + variables.toString());
+						var loader = new URLLoader();
+						loader.addEventListener(Event.COMPLETE, function(_) handler(Success(loader.data)));
+						loader.addEventListener(IOErrorEvent.IO_ERROR, function(e:IOErrorEvent) handler(Failure(new Error(e.toString()))));
+						loader.load(request);
+					});
+				}
+				catch (e:Error)
+					return Future.sync(Failure(e));
+			});
 		});
 	}
 	
@@ -103,12 +118,4 @@ class GoogleApi
 	private static var googleapi_get_token_jni = JNI.createStaticMethod ("org.haxe.extension.GoogleApi", "getToken", "(Lorg/haxe/lime/HaxeObject;Ljava/lang/String;)V");
 	private static var googleapi_invalidate_token_jni = JNI.createStaticMethod ("org.haxe.extension.GoogleApi", "invalidateToken", "(Ljava/lang/String;)V");
 	#end
-	
-	
-}
-
-@:enum
-abstract ActionKind(String)
-{
-	var TOKEN = "token";
 }
