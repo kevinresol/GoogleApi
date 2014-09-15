6a6628b95abca583f74edb392a96b7b0728a1128
Kevin Leung <kevinresol@gmail.com>
2014-09-15 22:30:25 +0800

    ios Fix

--- googleapi/native/Achievements.hx
+++ googleapi/native/Achievements.hx
@@ -1,5 +1,12 @@
 package googleapi.native;
+
+#if android
 import openfl.utils.JNI;
+#end
+
+#if ios
+import cpp.Lib;
+#end
 
 /**
  * https://developer.android.com/reference/com/google/android/gms/games/achievement/Achievements.html
@@ -11,26 +18,36 @@ class Achievements
 	
 	public static function increment(achievementId:String, numSteps:Int):Void
 	{
-		var f = JNI.createStaticMethod(ACHIEVEMENTS_CLASS_QUALIFIER, "increment", "(Ljava/lang/String;I)V");
-		f(achievementId, numSteps);
+		_increment(achievementId, numSteps);
 	}
 
 	public static function unlock(achievementId:String):Void
 	{
-		var f = JNI.createStaticMethod(ACHIEVEMENTS_CLASS_QUALIFIER, "unlock", "(Ljava/lang/String;)V");
-		f(achievementId);
+		_unlock(achievementId);
 	}
 	
 	public static function reveal(achievementId:String):Void
 	{
-		var f = JNI.createStaticMethod(ACHIEVEMENTS_CLASS_QUALIFIER, "reveal", "(Ljava/lang/String;)V");
-		f(achievementId);
+		_reveal(achievementId);
 	}
 
 	public static function setSteps(achievementId:String, numSteps:Int):Void
 	{
-		var f = JNI.createStaticMethod(ACHIEVEMENTS_CLASS_QUALIFIER, "setSteps", "(Ljava/lang/String;I)V");
-		f(achievementId, numSteps);
+		_setSteps(achievementId, numSteps);
 	}
+	
+	#if android
+	private var _increment = JNI.createStaticMethod(ACHIEVEMENTS_CLASS_QUALIFIER, "increment", "(Ljava/lang/String;I)V");
+	private var _unlock = JNI.createStaticMethod(ACHIEVEMENTS_CLASS_QUALIFIER, "unlock", "(Ljava/lang/String;)V");
+	private var _reveal = JNI.createStaticMethod(ACHIEVEMENTS_CLASS_QUALIFIER, "reveal", "(Ljava/lang/String;)V");
+	private var _setSteps = JNI.createStaticMethod(ACHIEVEMENTS_CLASS_QUALIFIER, "setSteps", "(Ljava/lang/String;I)V");
+	#end
+	
+	#if ios
+	private var _increment = null;
+	private var _unlock = null;
+	private var _reveal = null;
+	private var _setSteps = null;
+	#end
 	
 }
