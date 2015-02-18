289aca9ade3fdd4aef0a598850c626d0da7c18e9
Kevin Leung <kevinresol@gmail.com>
2015-02-18 19:08:26 +0800

    Simplify logic

--- dependencies/android/src/googleapi/GoogleApi.java
+++ dependencies/android/src/googleapi/GoogleApi.java
@@ -1,29 +1,24 @@
 package googleapi;
 
 import java.io.IOException;
+
 import android.util.Log;
-import android.accounts.AccountManager;
 import android.app.Activity;
-import android.content.res.AssetManager;
-import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.widget.Toast;
-import android.view.View;
 import com.google.android.gms.plus.Plus;
 import com.google.android.gms.plus.Plus.PlusOptions;
-import com.google.android.gms.games.Games;
 import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.auth.UserRecoverableAuthException;
 import com.google.android.gms.auth.GoogleAuthException;
-import com.google.android.gms.common.AccountPicker;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.example.games.basegameutils.GameHelper;
 import com.google.example.games.basegameutils.GameHelper.GameHelperListener;
 import org.haxe.lime.HaxeObject;
 import org.haxe.extension.Extension;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 // Google Ads
 import android.view.ViewGroup;
@@ -65,9 +60,9 @@ import com.google.android.gms.ads.*;
  * admob-related codes are from:
  * https://github.com/mkorman9/admob-openfl
  */
-public class GoogleApi extends Extension 
+public class GoogleApi extends Extension
 {
-	
+
 	// Google Ads
 	static RelativeLayout adLayout;
 	static RelativeLayout.LayoutParams adMobLayoutParams;
@@ -75,209 +70,191 @@ public class GoogleApi extends Extension
 	static Boolean adVisible = false, adInitialized = false, adTestMode = false;
 	static InterstitialAd interstitial;
 	////////////////////////////////////////////////////////////////////////
-	
-	protected static GameHelper mHelper;	
-	protected static HaxeObject mAccountNameHandler;
-	protected static HaxeObject mTokenHandler;
-	protected static HaxeObject mDebugHandler;
-	protected static String mTokenScope = "";
-	protected static String mAccountName = "";
-	
-	private static final String TAG = "GoogleApi";	
-	
-	private static final String INIT = "init";
-	private static final String TOKEN = "token";
-	private static final String ACCOUNT_NAME = "accountName";
-	private static final String SIGN_IN_SUCCEEDED = "signInSucceeded";
-	private static final String SIGN_IN_FAILED = "signInFailed";
-	
+
+	protected static GameHelper gameHelper;
+
+	protected static HaxeObject handler;
+	protected static String accountName = "";
+
+	private static final String TAG = "GoogleApi (Haxe)";
+
 	private static final int USER_RECOVERABLE_AUTH = 5;
-	private static final int ACCOUNT_PICKER = 2;
-	
+
 	public static GoogleApiClient getApiClient()
 	{
-		return mHelper.getApiClient();
+		return gameHelper.getApiClient();
 	}
-	
-	public static void init(HaxeObject accountNameHandler, HaxeObject debugHandler)
+
+	public static void authenticate(final String scopes, final HaxeObject handler)
 	{
-		mAccountNameHandler = accountNameHandler;
-		mDebugHandler = debugHandler;
-	}
-	
-	public static void getToken(HaxeObject handler, String scope)
-	{
-		Log.i(TAG, "getToken " + mAccountName);
-		if(mTokenHandler == null) //if not null => already processing another getToken request
+		Log.i(TAG, "authenticate " + scopes);
+		if (gameHelper == null)
 		{
-			if(mAccountName == "")
-				handler.call1("handler", "failed: GoogleAPI not yet ready"); // ACCOUNT_NAME not yet ready
-			else
+			Extension.mainActivity.runOnUiThread(new Runnable()
 			{
-				mTokenHandler = handler;
-				mTokenScope = scope;
-				new GetAuthToken().execute();
-			}
+				public void run()
+				{
+					Log.i(TAG, "Preparing GameHelper");
+
+					// create game helper with all APIs (Games, Plus, AppState):
+					gameHelper = new GameHelper(Extension.mainActivity, GameHelper.CLIENT_GAMES | GameHelper.CLIENT_PLUS /*| GameHelper.CLIENT_APPSTATE*/);
+
+					GameHelperListener listener = new GameHelper.GameHelperListener()
+					{
+						@Override
+						public void onSignInSucceeded()
+						{
+							Log.i(TAG, "Sign in Succeeded");
+							accountName = Plus.AccountApi.getAccountName(gameHelper.getApiClient());
+							new GetAuthToken(accountName, scopes, handler).execute();
+						}
+
+						@Override
+						public void onSignInFailed()
+						{
+							Log.i(TAG, "Sign in Failed");
+						}
+
+					};
+					gameHelper.setPlusApiOptions(PlusOptions.builder().build());
+					gameHelper.setup(listener);
+					gameHelper.beginUserInitiatedSignIn();
+
+				}
+			});
+		}
+		else
+		{
+			new GetAuthToken(accountName, scopes, handler).execute();
 		}
 	}
-	
+
 	public static void invalidateToken(String token)
 	{
 		GoogleAuthUtil.invalidateToken(Extension.mainContext, token);
 	}
-	
-	private static void trace(String message)
-	{
-		if(mDebugHandler != null)
-			mDebugHandler.call1("handler", message);
-	}
-	
-	
+
 	/**
-	 * Called when an activity you launched exits, giving you the requestCode 
-	 * you started it with, the resultCode it returned, and any additional data 
+	 * Called when an activity you launched exits, giving you the requestCode
+	 * you started it with, the resultCode it returned, and any additional data
 	 * from it.
 	 */
-	public boolean onActivityResult (int requestCode, int resultCode, Intent data) 
+	public boolean onActivityResult(int requestCode, int resultCode, Intent data)
 	{
-		if (requestCode == USER_RECOVERABLE_AUTH && resultCode == Activity.RESULT_OK) 
+		if (requestCode == USER_RECOVERABLE_AUTH && resultCode == Activity.RESULT_OK)
 		{
-			// Call it again returned from UserRecoverableAuthException 
-			new GetAuthToken().execute();
-		} 
-		else if (requestCode == USER_RECOVERABLE_AUTH && resultCode == Activity.RESULT_CANCELED) 
+			// Call it again returned from UserRecoverableAuthException
+			new GetAuthToken(accountName, data.getStringExtra("scopes"), handler).execute();
+			handler = null;
+		}
+		else if (requestCode == USER_RECOVERABLE_AUTH && resultCode == Activity.RESULT_CANCELED)
 		{
 			/*Toast.makeText(this, "User rejected authorization.",
 				Toast.LENGTH_SHORT).show();*/
 		}
-	   
-		mHelper.onActivityResult(requestCode, resultCode, data);
+
+		gameHelper.onActivityResult(requestCode, resultCode, data);
 		return true;
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Called when the activity is starting.
 	 */
-	public void onCreate (Bundle savedInstanceState) 
+	public void onCreate(Bundle savedInstanceState)
 	{
-		// create game helper with all APIs (Games, Plus, AppState):
-		mHelper = new GameHelper(Extension.mainActivity, GameHelper.CLIENT_GAMES | GameHelper.CLIENT_PLUS /*| GameHelper.CLIENT_APPSTATE*/);
-		mHelper.enableDebugLog(true);
 
-		// enable debug logs (if applicable)
-		/*if (DEBUG_BUILD) {
-			mHelper.enableDebugLog(true, "GameHelper");
-		}*/
-
-		GameHelperListener listener = new GameHelper.GameHelperListener() 
-		{
-			@Override
-			public void onSignInSucceeded() 
-			{
-				trace("signInSucceeded!");
-				mAccountName = Plus.AccountApi.getAccountName(mHelper.getApiClient());
-				
-				// throw it back to haxe
-				if(mAccountNameHandler != null)
-					mAccountNameHandler.call1("handler", mAccountName);
-			}
-			
-			@Override
-			public void onSignInFailed() 
-			{
-				
-			}
-
-		};
-		mHelper.setPlusApiOptions(PlusOptions.builder().build());
-		mHelper.setup(listener);
-		
 		// Google Ads
-		FrameLayout rootLayout = new FrameLayout(Extension.mainActivity); 
+		FrameLayout rootLayout = new FrameLayout(Extension.mainActivity);
 		adLayout = new RelativeLayout(Extension.mainActivity);
-        
-        RelativeLayout.LayoutParams adMobLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-		
-		((ViewGroup)Extension.mainView.getParent()).removeView(Extension.mainView);
-        rootLayout.addView(Extension.mainView);
+
+		RelativeLayout.LayoutParams adMobLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+
+		((ViewGroup) Extension.mainView.getParent()).removeView(Extension.mainView);
+		rootLayout.addView(Extension.mainView);
 		rootLayout.addView(adLayout, adMobLayoutParams);
-		
-        Extension.mainActivity.setContentView(rootLayout);
+
+		Extension.mainActivity.setContentView(rootLayout);
 		////////////////////////////////////////////////////////////////////////
 	}
-	
-	
+
+
 	/**
 	 * Perform any final cleanup before an activity is destroyed.
 	 */
-	public void onDestroy () 
+	public void onDestroy()
 	{
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Called as part of the activity lifecycle when an activity is going into
 	 * the background, but has not (yet) been killed.
 	 */
-	public void onPause () 
+	public void onPause()
 	{
-		if(adView != null)
+		if (adView != null)
 			adView.pause();
 	}
-	
-	
+
+
 	/**
-	 * Called after {@link #onStop} when the current activity is being 
+	 * Called after {@link #onStop} when the current activity is being
 	 * re-displayed to the user (the user has navigated back to it).
 	 */
-	public void onRestart () 
+	public void onRestart()
 	{
-		
+
 	}
-	
-	
+
+
 	/**
-	 * Called after {@link #onRestart}, or {@link #onPause}, for your activity 
+	 * Called after {@link #onRestart}, or {@link #onPause}, for your activity
 	 * to start interacting with the user.
 	 */
-	public void onResume ()
+	public void onResume()
 	{
-		if(adView != null)
+		if (adView != null)
 			adView.resume();
 	}
-	
-	
+
+
 	/**
-	 * Called after {@link #onCreate} &mdash; or after {@link #onRestart} when  
-	 * the activity had been stopped, but is now again being displayed to the 
+	 * Called after {@link #onCreate} &mdash; or after {@link #onRestart} when
+	 * the activity had been stopped, but is now again being displayed to the
 	 * user.
 	 */
-	public void onStart () 
+	public void onStart()
 	{
-		mHelper.onStart(Extension.mainActivity);
+		if (gameHelper != null)
+			gameHelper.onStart(Extension.mainActivity);
 	}
-	
-	
+
+
 	/**
-	 * Called when the activity is no longer visible to the user, because 
-	 * another activity has been resumed and is covering this one. 
+	 * Called when the activity is no longer visible to the user, because
+	 * another activity has been resumed and is covering this one.
 	 */
-	public void onStop () 
+	public void onStop()
 	{
-		mHelper.onStop();
+		if (gameHelper != null)
+			gameHelper.onStop();
 	}
-	
-	
-	
-	
-	static class GetAuthToken extends AsyncTask<Void, Void, String> 
+
+
+	static class GetAuthToken extends AsyncTask<String, Void, String>
 	{
-		public GetAuthToken() 
+		private String accountName;
+		private String scopes;
+		private HaxeObject handler;
+
+		public GetAuthToken(String accountName, String scopes, HaxeObject handler)
 		{
-			
+			this.accountName = accountName;
+			this.scopes = scopes;
+			this.handler = handler;
 		}
 		
 		/*@Override
@@ -285,55 +262,81 @@ public class GoogleApi extends Extension
 		{
 		
 		}*/
-		
+
 		@Override
-		protected String doInBackground(Void... params) 
+		protected String doInBackground(String... params)
 		{
-			try 
+			try
 			{
-				String token = GoogleAuthUtil.getToken(Extension.mainActivity, mAccountName, "oauth2:" + mTokenScope);
+				String token = GoogleAuthUtil.getToken(Extension.mainActivity, accountName, "oauth2:" + scopes);
 				return token;
-			} 
-			catch (UserRecoverableAuthException userRecoverableException) 
+			}
+			catch (UserRecoverableAuthException userRecoverableException)
 			{
-				Extension.mainActivity.startActivityForResult(userRecoverableException.getIntent(), USER_RECOVERABLE_AUTH);
-			} 
+				Intent intent = userRecoverableException.getIntent();
+				intent.putExtra("scopes", scopes);
+				GoogleApi.handler = handler; // hack: save the handler reference at this point because it cannot be passed to another activity through the intent
+				Extension.mainActivity.startActivityForResult(intent, USER_RECOVERABLE_AUTH);
+			}
 			catch (IOException ioEx)
 			{
 				return "failed: IO Error - " + ioEx.getMessage();
 			}
-			catch (GoogleAuthException authEx) 
+			catch (GoogleAuthException authEx)
 			{
 				return "failed: GoogleAuthException - " + authEx.getMessage();
 			}
 			return null;
 		}
-		
+
 		@Override
-		protected void onPostExecute(String resultToken) 
+		protected void onPostExecute(String resultToken)
 		{
-			if (resultToken != null)
+			try
 			{
-				mTokenHandler.call1("handler", resultToken);
-				mTokenHandler = null;
-				mTokenScope = "";
+				if (resultToken != null)
+				{
+
+					JSONObject obj = new JSONObject();
+					obj.put("status", "success");
+					obj.put("scopes", scopes);
+					obj.put("accessToken", resultToken);
+					handler.call1("handle", obj.toString());
+					return;
+				}
+				else
+				{
+					handler.call1("handle", "{\"status\":\"failure\", \"error\":\"no token returned\"}");
+					return;
+				}
 			}
+			catch (JSONException ex)
+			{
+				handler.call1("handle", "{\"status\":\"failure\", \"error\":\"" + ex.toString() + "\"}");
+				return;
+			}
+
 		}
 	}
-	
+
 	////////////////////////////////////////////////////////////////////////
-	static public void loadAd() {
+	static public void loadAd()
+	{
 		AdRequest adRequest = new AdRequest.Builder().build();
 		adView.loadAd(adRequest);
 	}
-	
-	static public void initAd(final String id, final int x, final int y, final int size, final boolean testMode) {
-		Extension.mainActivity.runOnUiThread(new Runnable() {
-			public void run() {
+
+	static public void initAd(final String id, final int x, final int y, final int size, final boolean testMode)
+	{
+		Extension.mainActivity.runOnUiThread(new Runnable()
+		{
+			public void run()
+			{
 				String adID = id;
 				adTestMode = testMode;
-				
-				if (Extension.mainActivity == null) {
+
+				if (Extension.mainActivity == null)
+				{
 					return;
 				}
 
@@ -342,38 +345,48 @@ public class GoogleApi extends Extension
 				adView.setAdSize(AdSize.SMART_BANNER);
 
 				loadAd();
-				adMobLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT); 
-       
-                if(x == 0) {
+				adMobLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+				if (x == 0)
+				{
 					adMobLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-                }
-				else if(x == 1) {
+				}
+				else if (x == 1)
+				{
 					adMobLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-                }
-				else if(x == 2) {
+				}
+				else if (x == 2)
+				{
 					adMobLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-                }
-				
-				if(y == 0) {
+				}
+
+				if (y == 0)
+				{
 					adMobLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
 				}
-				else if(y == 1) {
+				else if (y == 1)
+				{
 					adMobLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
 				}
-				else if(y == 2) {
+				else if (y == 2)
+				{
 					adMobLayoutParams.addRule(RelativeLayout.CENTER_VERTICAL);
-                }
-				
+				}
+
 				adInitialized = true;
 			}
 		});
 	}
-	
-	static public void showAd() {
+
+	static public void showAd()
+	{
 		Log.i("GoogleApi", "showAd");
-		Extension.mainActivity.runOnUiThread(new Runnable() {
-			public void run() {
-				if (adInitialized && !adVisible) {
+		Extension.mainActivity.runOnUiThread(new Runnable()
+		{
+			public void run()
+			{
+				if (adInitialized && !adVisible)
+				{
 					adLayout.removeAllViews();
 					adView.setBackgroundColor(Color.BLACK);
 					adLayout.addView(adView, adMobLayoutParams);
@@ -383,11 +396,15 @@ public class GoogleApi extends Extension
 			}
 		});
 	}
-        
-	static public void hideAd() {
-		Extension.mainActivity.runOnUiThread(new Runnable() {
-			public void run() {
-				if (adInitialized && adVisible) {
+
+	static public void hideAd()
+	{
+		Extension.mainActivity.runOnUiThread(new Runnable()
+		{
+			public void run()
+			{
+				if (adInitialized && adVisible)
+				{
 					adLayout.removeAllViews();
 					loadAd();
 					adVisible = false;
@@ -395,37 +412,46 @@ public class GoogleApi extends Extension
 			}
 		});
 	}
-	
-	static public void loadInterstitial() {
+
+	static public void loadInterstitial()
+	{
 		AdRequest adRequest = new AdRequest.Builder().build();
 		interstitial.loadAd(adRequest);
 	}
-	
-	static public void initInterstitial(final String id, final boolean testMode) {
-        Extension.mainActivity.runOnUiThread(new Runnable() {
-            public void run() {
-				if (Extension.mainActivity == null) {
+
+	static public void initInterstitial(final String id, final boolean testMode)
+	{
+		Extension.mainActivity.runOnUiThread(new Runnable()
+		{
+			public void run()
+			{
+				if (Extension.mainActivity == null)
+				{
 					return;
 				}
-				
-                interstitial = new InterstitialAd(Extension.mainActivity);
-                interstitial.setAdUnitId(id);
 
-                loadInterstitial();
-            }
-        });
-    }
+				interstitial = new InterstitialAd(Extension.mainActivity);
+				interstitial.setAdUnitId(id);
 
-    static public void showInterstitial() {
-        Extension.mainActivity.runOnUiThread(new Runnable() {
-            public void run() {
-                if (interstitial.isLoaded()) {
-                    interstitial.show();
-                }
-            }
-        });
-    }
+				loadInterstitial();
+			}
+		});
+	}
+
+	static public void showInterstitial()
+	{
+		Extension.mainActivity.runOnUiThread(new Runnable()
+		{
+			public void run()
+			{
+				if (interstitial.isLoaded())
+				{
+					interstitial.show();
+				}
+			}
+		});
+	}
 	///////////////////////////////////////////////////////////////////////////////////////////
-	
-	
+
+
 }
--- googleapi/rest/Rest.hx
+++ googleapi/rest/Rest.hx
@@ -1,10 +1,10 @@
 package googleapi.rest;
+import googleapi.GoogleApi.AccessToken;
 import haxe.Json;
 import openfl.events.Event;
 import openfl.events.IOErrorEvent;
 import openfl.net.URLLoader;
 import openfl.net.URLRequest;
-import openfl.net.URLRequestMethod;
 import openfl.net.URLVariables;
 using tink.CoreApi;
 /**
@@ -18,30 +18,10 @@ class Rest
 	
 	public static function callRaw(scope:String, url:String, ?variables:URLVariables, method:String = "GET"):Surprise<String, Error>
 	{
-		//TODO cache
-		//ready -> getToken -> load url
-		return GoogleApi.getToken(scope).flatMap(function(outcome)	
-		{
-			try
-			{
-				if (variables == null)
-					variables = new URLVariables();
-				
-				variables.access_token = outcome.sure();
-				return Future.async(function(handler)
-				{
-					var request = new URLRequest(url + "?" + variables.toString());
-					request.method = method;
-					
-					var loader = new URLLoader();
-					loader.addEventListener(Event.COMPLETE, function(_) handler(Success(loader.data)));
-					loader.addEventListener(IOErrorEvent.IO_ERROR, function(e:IOErrorEvent) handler(Failure(new Error(e.toString()))));
-					loader.load(request);
-				});
-			}
-			catch (e:Error) // token error (e.g. typo in scope)
-				return Future.sync(Failure(e));
-		});
+		if (GoogleApi.token == null)
+			return GoogleApi.authenticate().flatMap(tokenNext.bind(_, scope, url, variables, method));
+		else
+			return GoogleApi.token.flatMap(tokenNext.bind(_, scope, url, variables, method));
 	}
 	
 	public static function call<T>(scope:String, url:String, ?variables:URLVariables, method:String = "GET"):Surprise<T, Error>
@@ -59,6 +39,38 @@ class Rest
 			catch (e:Error)
 				return Failure(e);
 		});
+	}
+	
+	private static function tokenNext(outcome:Outcome<AccessToken, Error>, scope, url, ?variables, method )
+	{
+		try
+		{
+			var accessToken = outcome.sure();
+			if (accessToken.scopes.indexOf(scope) == -1)
+			{
+				var scopes = accessToken.scopes.concat([scope]);
+				scopes.remove(GoogleApi.SCOPE_GAMES);
+				scopes.remove(GoogleApi.SCOPE_PLUS_LOGIN);
+				return GoogleApi.authenticate(scopes).flatMap(tokenNext.bind(_, scope, url, variables, method));
+			}
+			
+			if (variables == null)
+				variables = new URLVariables();
+			
+			variables.access_token = outcome.sure().token;
+			return Future.async(function(handler)
+			{
+				var request = new URLRequest(url + "?" + variables.toString());
+				request.method = method;
+				
+				var loader = new URLLoader();
+				loader.addEventListener(Event.COMPLETE, function(_) handler(Success(loader.data)));
+				loader.addEventListener(IOErrorEvent.IO_ERROR, function(e:IOErrorEvent) handler(Failure(new Error(e.toString()))));
+				loader.load(request);
+			});
+		}
+		catch (e:Error) // token error (e.g. typo in scope)
+			return Future.sync(Failure(e));
 	}
 	
 }
